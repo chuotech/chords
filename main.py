0d2ae306b56c2469149667a5f96a21a83c77d1a7
chuotech <chuotech2003@gmail.com>
2025-02-04 22:29:43 -0500

    added get chord with interval

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from music21 import converter, chord
 import time
 import pretty_midi
+import pprint
 def get_notes(midi_file_pm):
     notes = []
     for instrument in midi_file_pm.instruments:
@@ -33,15 +34,65 @@ def get_etimated_tempo(midi_file_pm):
     else:
         print("No tempo found")
         return
-# def strum_pattern(midi_stream):
+def get_full_chord_output(notes, duration, tempo):
+    beats_total = int((tempo/60)*duration)
+    quarter_length = duration/beats_total
+    sixteenth = quarter_length/4
+    # print(beats_total)
+    chord_list = [beats_total]
+    # curr_note = notes[0]
+    curr_highest_pitch = 0
+    curr_lowest_pitch = 10000
+    curr_chord = []
+    curr_highest = 0
+    curr_lowest = 100000
+    curr_max_interval_length = quarter_length - sixteenth
+    i = 0
+    for interval in range(beats_total):
+        while i < len(notes) and (notes[i]["onset"] <= curr_max_interval_length):
+            curr_chord.append(notes[i]["pitch"])
+            if notes[i]["pitch"] > curr_highest_pitch:
+                curr_highest = notes[i]["onset"]
+                curr_highest_pitch = notes[i]["pitch"]
+            if notes[i]["pitch"] < curr_lowest_pitch:
+                curr_lowest = notes[i]["onset"]
+                curr_lowest_pitch = notes[i]["pitch"]
+            print(curr_highest, curr_lowest)
+            i += 1
+        chord_list.append((chord.Chord(curr_chord).pitchedCommonName, "quarter note "+str(interval + 1), str(get_strum(curr_lowest, curr_highest))))
+        # prev_chord = chord.Chord(curr_chord).pitchedCommonName
+        curr_max_interval_length += quarter_length
+        curr_lowest = 0
+        curr_highest = 0
+        if i < len(notes) and curr_max_interval_length > notes[i]["onset"]:
+            curr_chord = []
+            curr_highest = 0
+            curr_lowest = 100000
+            curr_highest_pitch = 0
+            curr_lowest_pitch = 10000
+    return chord_list
+def get_strum(lowest, highest):
+    if highest > lowest:
+        return "DOWN"
+    elif lowest > highest:
+        return "UP"
+    else:
+        return "HOLD"
+
 start = time.time()
-midi_file = 'major1.mid'  # Replace with the path to your MIDI file
+midi_file = 'strums_kinda.mid'  # Replace with the path to your MIDI file
 midi_file_pm = pretty_midi.PrettyMIDI(midi_file)
 midi_stream = converter.parse(midi_file)
 midi_notes = get_notes(midi_file_pm)
+file_duration = midi_file_pm.get_end_time()
 key_signature = get_key_signature(midi_stream)
-end = time.time()
+notes = get_notes(midi_file_pm)
+tempo = get_etimated_tempo(midi_file_pm)
 print("Detected Key Signature:", key_signature)
-get_chords(midi_stream)
-get_etimated_tempo(midi_file_pm)
+# get_chords(midi_stream)
+print(tempo)
+print(file_duration)
+pprint.pp(notes)
+pprint.pp(get_full_chord_output(notes, file_duration, tempo))
+end = time.time()
 print(str(end - start)+" secs")
